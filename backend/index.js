22b001b9c42e25863e35a3486ace08d06ed0df35
neelpatel1144 <patelneel1144@gmail.com>
2025-02-19 11:01:14 +0530

    Update index.js
    used env

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,19 @@ const app = express();
 app.use(express.json());
 const cors = require("cors")
 const sequelize = require('./src/db/db.js');
-// connectDB()
 const mainRoute = require("./src/routes/index.js")
 app.use(
   cors({
-    origin: ["http://localhost:3000/","http://localhost:3000"],
+    origin: process.env.ORIGINS,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Custom-Header"],
-    // allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
   })
 );
 sequelize.sync();
 
 app.use("/", mainRoute)
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("listening on port" + port);
 });
